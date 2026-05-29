a2a84389362da4f5b8f16b089cd0dc6e2d038ac0
Abdul Charis Al Fikri <143812909+dotcomspace@users.noreply.github.com>
2026-05-29 22:18:07 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use App\Http\Controllers\SentimenController;
 
 // Halaman utama
 Route::get('/', [SentimenController::class, 'index']);
-
-// API routes dipanggil dari JavaScript di Blade
-Route::prefix('api/v1')->group(function () {
-    Route::post('/analyze',  [SentimenController::class, 'analyze']);
-    Route::get('/news',      [SentimenController::class, 'news']);
-    Route::get('/history',   [SentimenController::class, 'historyApi']);
-    Route::get('/stats',     [SentimenController::class, 'stats']);
-});
